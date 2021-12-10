12bee5db630d821f79184216d75e0d71a2b5da51
Ronaldabr <93811453+Ronaldabr@users.noreply.github.com>
2021-12-10 09:08:15 +0530

    🙂

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -841,8 +841,7 @@ async def advantage_spell_chok(msg):
         for mov in gs_parsed:
             imdb_s = await get_poster(mov.strip(), bulk=True) # searching each keyword in imdb
             if imdb_s:
-                movielist += [movie.get('title') for movie in imdb_s]
-  movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
+                movielist += [movie.get('title') for movie in imdb    
     movielist = list(dict.fromkeys(movielist)) # removing duplicates
     if not movielist:
         k = await msg.reply("I couldn't find anything related to that. Check your spelling")
