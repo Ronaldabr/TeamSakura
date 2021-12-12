2ef5cea129aba297834a97f4734b412e8a2ee566
Ronaldabr <93811453+Ronaldabr@users.noreply.github.com>
2021-12-12 12:16:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -123,7 +123,7 @@ async def next_page(bot, query):
             [InlineKeyboardButton("« BACK", callback_data=f"next_{req}_{key}_{off_set}"), InlineKeyboardButton(f"📃 Pages {round(int(offset)/10)+1} / {round(total/10)}", callback_data="pages")]
         )
     elif off_set is None:
-        btn.append([InlineKeyboardButton(f"🗓 {round(int(offset)/10)+1} / {round(total/10)}", callback_data="pages"), InlineKeyboardButton("NEXT ⏩", callback_data=f"next_{req}_{key}_{n_offset}")])
+        btn.append([InlineKeyboardButton(f"🗓 {round(int(offset)/10)+1} / {round(total/10)}", callback_data="pages"), InlineKeyboardButton("MORE FILES »", callback_data=f"next_{req}_{key}_{n_offset}")])
     else:
         btn.append(
             [
