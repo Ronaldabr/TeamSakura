a8adff0098a71c8a2f736365fe258a8d87b0b9de
Ronaldabr <93811453+Ronaldabr@users.noreply.github.com>
2022-01-13 12:15:27 +0530

    😎

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -753,13 +753,6 @@ async def auto_filter(client, msg, spoll=False):
             if not files:
                 if SPELL_CHECK_REPLY:
                     return await advantage_spell_chok(msg)
-                else:
-                    return
-        else:
-            return
-    else:
-        message = msg.message.reply_to_message # msg will be callback query
-        search, files, offset, total_results = spoll
         if SINGLE_BUTTON:
             btn = [
                 [
